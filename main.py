e646fc7ca6f70110be84364152857ecded4f0f5a
MerotecDev <merotec32@gmail.com>
2026-03-28 23:13:04 -0300

    Aualização de importações

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ async def main(page: ft.Page):
                         ft.ElevatedButton(
                         "FOTO", 
                         icon=ft.icons.CAMERA_ALT, 
-                        on_click=picker.pick_files_async
+                        on_click=picker.pick_files_async(upload_data=True)
                         ),
                         img_previa
                     ], alignment=ft.MainAxisAlignment.START),
